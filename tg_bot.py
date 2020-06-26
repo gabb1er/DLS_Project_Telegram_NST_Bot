10ab56ab47849aff4e975f14599760d64f267096
Ubuntu <ubuntu@ip-172-31-40-207.eu-west-2.compute.internal>
2020-06-26 19:12:46 +0000

    Changed logic of the bot. Example of usage is added.

--- tg_bot.py
+++ tg_bot.py
@@ -3,6 +3,7 @@ This module describes TG bot
 for style transfer
 """
 
+import os
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.message import ContentType
@@ -14,11 +15,11 @@ from config import TOKEN
 from bot_messages import messages
 from style_transfer import image_style_transfer
 
+
 class RequestOrder(StatesGroup):
     content_inp = State()
     style_inp = State()
     gamma_inp = State()
-    resize_inp = State()
 
 
 # Initialize bot and dispatcher
@@ -35,7 +36,13 @@ async def run_nst(inp):
     style_img_name = str(inp['user_id']) + '_style_img.jpg'
     await bot.download_file_by_id(inp['content_img_id'], destination=content_img_name)
     await bot.download_file_by_id(inp['style_img_id'], destination=style_img_name)
-    await image_style_transfer(inp['gamma'], inp['resize'], str(inp['user_id']))
+    await image_style_transfer(inp['gamma'], str(inp['user_id']))
+
+
+async def remove_images(user_id):
+    os.remove(user_id + '_content_img.jpg')
+    os.remove(user_id + '_style_img.jpg')
+    os.remove(user_id + '_output_img.jpg')
 
 
 @dp.message_handler(commands=['start'], state="*")
@@ -45,6 +52,7 @@ async def process_start_command(message: types.Message):
     """
     await message.answer(messages['info'])
 
+
 @dp.message_handler(commands=['help'], state="*")
 async def process_help_command(message: types.Message):
     """
@@ -52,12 +60,14 @@ async def process_help_command(message: types.Message):
     """
     await message.answer(messages['help'])
 
+
 @dp.message_handler(commands=['info'], state="*")
 async def process_info_command(message: types.Message):
     """
     This handler will be called when user sends `/info` command
     """
     await message.answer(messages['info'])
+
 
 @dp.message_handler(commands=['cancel'], state="*")
 async def end_session(message: types.Message,
@@ -68,6 +78,22 @@ async def end_session(message: types.Message,
     await state.finish()
     await message.answer(messages['end_session'])
 
+
+@dp.message_handler(commands=['example'], state="*")
+async def end_session(message: types.Message):
+    """
+    This handler will be called when user sends /example command
+    """
+    # send result to the user:in
+    await types.ChatActions.upload_photo()
+    media = types.MediaGroup()
+    media.attach_photo(types.InputFile('example_content_img.jpg'), 'Content Image')
+    media.attach_photo(types.InputFile('example_style_img.jpg'), 'Style Image')
+    media.attach_photo(types.InputFile('example_output_img.jpg'), 'Output Image')
+    await message.answer(messages['example'])
+    await message.answer_media_group(media=media)
+
+
 @dp.message_handler(commands=['nst'], state="*")
 async def start_nst(message: types.Message):
     """
@@ -77,7 +103,8 @@ async def start_nst(message: types.Message):
     await message.answer(messages['nst'])
     await RequestOrder.content_inp.set()
 
-@dp.message_handler(state=RequestOrder.content_inp, content_types=ContentType.ANY) #=ContentType.PHOTO)
+
+@dp.message_handler(state=RequestOrder.content_inp, content_types=ContentType.ANY)  # =ContentType.PHOTO)
 async def get_content_image(message: types.Message,
                             state: FSMContext):
     """
@@ -109,6 +136,7 @@ async def get_style_image(message: types.Message,
     await RequestOrder.next()
     await message.answer(messages['gamma_inp'])
 
+
 @dp.message_handler(state=RequestOrder.gamma_inp, content_types=ContentType.TEXT)
 async def set_gamma(message: types.Message,
                     state: FSMContext):
@@ -118,50 +146,37 @@ async def set_gamma(message: types.Message,
     """
     if message.text.lower() == 'd':
         await state.update_data(gamma=1.0)
-        await RequestOrder.next()
-        await message.answer(messages['resize_inp'])
     else:
         try:
             gamma_val = float(message.text)
             if gamma_val > 0 and gamma_val <= 10:
                 await state.update_data(gamma=gamma_val)
-                await RequestOrder.next()
-                await message.answer(messages['resize_inp'])
             else:
                 await message.answer(messages['gamma_wrong_inp'])
+                return
         except ValueError:
             await message.answer(messages['gamma_wrong_inp'])
+            return
 
-@dp.message_handler(state=RequestOrder.resize_inp, content_types=ContentType.TEXT)
-async def set_resize(message: types.Message,
-                    state: FSMContext):
-    """
-    This handler will be called during inputs collection dialog,
-    when user tells if image has to be resized.
-    """
-    if message.text.lower() not in {'y', 'n'}:
-        await message.answer(messages['resize_wrong_inp'])
-        return
-    else:
-        is_resize = message.text.lower() == 'y'
-        await state.update_data(resize=is_resize)
-        await state.update_data(user_id=message.from_user.id)
-        await message.answer(messages['inputs_received'])
+    await message.answer(messages['gamma_is_set'])
+    await state.update_data(user_id=message.from_user.id)
+    await message.answer(messages['inputs_received'])
 
-        # get collected input data:
-        inputs = await state.get_data()
+    # get collected input data:
+    inputs = await state.get_data()
+    # run style transfer:
+    await message.answer(messages['processing'])
+    await run_nst(inputs)
 
-        # run style transfer:
-        await message.answer(messages['processing'])
-        await run_nst(inputs)
+    # send result to the user:in
+    await types.ChatActions.upload_photo()
+    media = types.MediaGroup()
+    output_img_name = str(inputs['user_id']) + '_output_img.jpg'
+    media.attach_photo(types.InputFile(output_img_name), messages['finish'])
+    await message.answer_media_group(media=media)
+    await remove_images(str(inputs['user_id']))
+    await state.finish()
 
-        # send result to the user:in
-        await types.ChatActions.upload_photo()
-        media = types.MediaGroup()
-        output_img_name = str(inputs['user_id']) + '_output_img.jpg'
-        media.attach_photo(types.InputFile(output_img_name), messages['finish'])
-        await message.answer_media_group(media=media)
-        await state.finish()
 
 @dp.message_handler(content_types=ContentType.ANY, state="*")
 async def unspecified_message(message: types.Message):
